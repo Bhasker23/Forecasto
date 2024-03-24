fa9819bfe5c7dee2b1aaf8885d316edcaac4c1ae
Bhasker23 <shrimalibhaskar2398@gmail.com>
2024-03-24 20:11:11 +0530

    Forecasto folder structure created

---
+++ Forecasto/src/main/java/com/forecasto/ForecastoApplication.java
@@ -0,0 +1,13 @@
+package com.forecasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastoApplication.class, args);
+	}
+
+}
---
+++ Forecasto/src/test/java/com/forecasto/ForecastoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forecasto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
