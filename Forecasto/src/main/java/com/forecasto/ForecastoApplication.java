cea1794d93e041730c623ddc296eafee277b1087
Bhasker23 <shrimalibhaskar2398@gmail.com>
2024-03-26 14:50:14 +0530

    Forecasto backend updated with url and fronted code added.

--- Forecasto/src/main/java/com/forecasto/ForecastoApplication.java
+++ Forecasto/src/main/java/com/forecasto/ForecastoApplication.java
@@ -11,3 +11,6 @@ public class ForecastoApplication {
 	}
 
 }
+
+//https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=1204d1db8a4c17b9fc0a1f4902ede7a6
+//https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=1204d1db8a4c17b9fc0a1f4902ede7a6
